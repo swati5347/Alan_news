fa433d4cd93e036390b5202b068d8b0347bcf49f
swati5347 <76245363+swati5347@users.noreply.github.com>
2020-12-19 14:52:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# alan_app_chitkara
+You can try my react applivation using this link: https://itisnotlegal.github.io/alan_app_chitkara/
+or
+you can download my app name "AlanNews.apk"
+
---
+++ precache-manifest.8bac8f4e124bde2f8ef567a966d7a102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935fc178685adc5babfc2694abe21188",
+    "url": "/alan_app_chitkara/index.html"
+  },
+  {
+    "revision": "c23b181041302a12f006",
+    "url": "/alan_app_chitkara/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "b4b9c615139b3ada1c25",
+    "url": "/alan_app_chitkara/static/js/2.21aad379.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan_app_chitkara/static/js/2.21aad379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23b181041302a12f006",
+    "url": "/alan_app_chitkara/static/js/main.27837377.chunk.js"
+  },
+  {
+    "revision": "156a7832a512e9912000",
+    "url": "/alan_app_chitkara/static/js/runtime-main.396c7a1e.js"
+  },
+  {
+    "revision": "4f8514a5bbfb183cca061932f2f250eb",
+    "url": "/alan_app_chitkara/static/media/logo.4f8514a5.png"
+  }
+]);
